53b03c104665acd8bbe84e2d3f040da61bc5d3a3
futrmain <futrmain@gmail.com>
2026-01-12 21:39:15 +0100

    added generalized sylvester layer and network with jacobian unit tests

--- tests/test_layers.py
+++ tests/test_layers.py
@@ -1,7 +1,7 @@
 import pytest
 import torch
 
-from sylvestorch.layers import SylvesterBlock
+from sylvestorch.layers import SylvesterBlock, AutoRecursiveFunction, GeneralizedSylvesterBlock
 
 @pytest.mark.parametrize("input_size,hidden_size,ortho_eps", [(4, 3, 1e-6), (8, 8, 1e-3), (123, 13, 1e-4)])
 def test_initialization(input_size, hidden_size, ortho_eps):
@@ -90,3 +90,63 @@ def test_inverse(batch, input_size, hidden_size, ortho_eps):
     assert error_max <= 1e-6, f"Error max: {error_max}"
     assert iteration < 99, f"Number of iterations: {iteration}"
     assert torch.allclose(x, z, atol=1e-6), f"Inverse not equal to original, Number of iterations: {iteration}, Error max: {error_max}"
+
+
+@pytest.mark.parametrize("batch,input_size", [(100, 3), 
+                                              (100, 30), (100, 80), (1000,5)
+                                              ])
+def test_log_det_AutoRecursiveFunction_correct(batch, input_size):
+    def jacobian(function, input):
+        x = input.detach().requires_grad_(True)
+        y, logdet = function(x)
+            
+        J = torch.zeros(x.shape[0], x.shape[1], x.shape[1], dtype=x.dtype) # batch x out_s x in_s
+        auto_grad = torch.autograd.functional.jacobian(function, x)[0]
+        for i in range(J.shape[0]):
+            for j in range(J.shape[1]):
+                J[i,j,:] = auto_grad[i, j, i, :]
+        return J, logdet
+    
+    
+    far = AutoRecursiveFunction(input_size).double()
+
+    x = torch.randn(batch, input_size, dtype=torch.double, requires_grad=True)
+
+    # Analytic output
+    J, logdet = jacobian(far.forward, x)
+    logdet = torch.sum(torch.log(torch.abs(logdet)), dim=1)
+    detJ = torch.log(torch.abs(torch.det(J)))
+    
+    # The flow should be orientation-preserving often, but abs keeps this robust.
+    assert torch.isfinite(logdet).all(), "Infinite values"
+    assert not torch.isnan(logdet).any(), "NaN values"
+    assert torch.allclose(detJ, logdet, atol=1e-4, rtol=1e-4), f"{detJ}\n{logdet}"
+
+@pytest.mark.parametrize("batch,input_size", [(100, 3), 
+                                              (100, 30), (100, 80), (1000,5)
+                                              ])
+def test_log_det_GeneralizedSylvesterBlock_correct(batch, input_size):
+    def jacobian(function, input):
+        x = input.detach().requires_grad_(True)
+        y, logdet = function(x)
+            
+        J = torch.zeros(x.shape[0], x.shape[1], x.shape[1], dtype=x.dtype) # batch x out_s x in_s
+        auto_grad = torch.autograd.functional.jacobian(function, x)[0]
+        for i in range(J.shape[0]):
+            for j in range(J.shape[1]):
+                J[i,j,:] = auto_grad[i, j, i, :]
+        return J, logdet
+    
+    
+    block = GeneralizedSylvesterBlock(input_size).double()
+
+    x = torch.randn(batch, input_size, dtype=torch.double, requires_grad=True)
+
+    # Analytic output
+    J, logdet = jacobian(block.forward, x)
+    detJ = torch.log(torch.abs(torch.det(J)))
+    
+    # The flow should be orientation-preserving often, but abs keeps this robust.
+    assert torch.isfinite(logdet).all(), "Infinite values"
+    assert not torch.isnan(logdet).any(), "NaN values"
+    assert torch.allclose(detJ, logdet, atol=1e-3, rtol=1e-4), f"{detJ}\n{logdet}"
